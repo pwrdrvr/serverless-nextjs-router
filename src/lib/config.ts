d09b4bfb9aa9d41b7185c1d38d76d379d3302b6f
Harold <harold@pwrdrvr.com>
2021-06-26 20:30:25 -0400

    Configs from env vars

---
+++ src/lib/config.ts
@@ -0,0 +1,50 @@
+export interface IConfig {
+  region: string;
+  s3BucketName: string;
+  s3DomainName: string;
+}
+
+export class Config implements IConfig {
+  private static _instance: Config;
+  public static get instance(): IConfig {
+    if (Config._instance === undefined) {
+      Config._instance = new Config();
+    }
+    return Config._instance;
+  }
+
+  private constructor() {
+    this._region = process.env.AWS_REGION || 'MicroApps';
+    this._s3BucketName = process.env.S3BUCKETNAME || 'MicroApps';
+    this._s3DomainName = '';
+    const regionInfix = this._region === 'us-east-1' ? '' : `${this._region}.`;
+    this._s3DomainName = `${this._s3BucketName}.s3.${regionInfix}amazonaws.com`;
+  }
+
+  public static get envLevel(): 'dev' | 'qa' | 'prod' | 'local' {
+    const nodeEnv = process.env.NODE_ENV || 'dev';
+    if (nodeEnv.startsWith('prod')) {
+      return 'prod';
+    } else if ((nodeEnv as string) === 'qa') {
+      return 'qa';
+    } else if ((nodeEnv as string) === 'local') {
+      return 'local';
+    }
+    return 'dev';
+  }
+
+  private _region: string;
+  public get region(): string {
+    return this._region;
+  }
+
+  private _s3BucketName: string;
+  public get s3BucketName(): string {
+    return this._s3BucketName;
+  }
+
+  private _s3DomainName: string;
+  public get s3DomainName(): string {
+    return this._s3DomainName;
+  }
+}
